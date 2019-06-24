99e88385043ee5ffe185ed6aff7b277c66f45ba3
Alejandro De Santiago <adesa002@ucr.edu>
2019-06-24 12:53:31 -0700

    italicize genus names fig 4

--- R-Scripts/03-ASV-OTU_Comparisons/03C-Desmoscolecidae_OTU-ASV_Comparison.R
+++ R-Scripts/03-ASV-OTU_Comparisons/03C-Desmoscolecidae_OTU-ASV_Comparison.R
@@ -164,45 +164,47 @@ DF_VSearch$SequenceSamples <- paste(DF_VSearch$Seqs, DF_VSearch$Sample)
 DF_DADA2$RelAbundance <- round(DF_DADA2$RelAbundance)
 DF_VSearch$RelAbundance <- round(DF_VSearch$RelAbundance)
 
-Final_DF <- merge(DF_VSearch, DF_DADA2, by.x = c("SequenceSamples"), by.y = c("SequenceSamples"), all.x = T, all.y = T)
-Label_DF <- as.data.frame(na.omit(Final_DF))
+### Merge Table and Remove NAs ###
+DADA2_Final <- subset(DF_DADA2, select = c(NewName, RelAbundance, Species, Method, SequenceSamples, Sample))
+names(DADA2_Final) <- c("ASV", "DADA2_Abundance", "Species", "DADA2_Method", "SequenceSamples", "Sample")
+VSearch_Final <- subset(DF_VSearch, select = c(NewName, RelAbundance, Species, Method, SequenceSamples, Sample))
+names(VSearch_Final) <- c("OTU", "VSearch_Abundance", "Species", "VSearch_Method", "SequenceSamples", "Sample")
 
-Label_DF_left <- subset(Label_DF, select=c("NewName.x", "RelAbundance.x"))
-Label_DF_left_Format <- as.data.frame(Label_DF_left %>% group_by(RelAbundance.x) %>% mutate(Order = seq_along(RelAbundance.x)) %>% spread(key = Order, value = NewName.x))
+Final_DF <- merge(DADA2_Final, VSearch_Final, by.x = c("SequenceSamples"), by.y = c("SequenceSamples"), all.x = T, all.y = T)
+Final_DF <- as.data.frame(na.omit(Final_DF))
 
-Label_DF_right <- subset(Label_DF, select=c("NewName.y", "RelAbundance.y"))
-Label_DF_right_Format <- as.data.frame(Label_DF_right %>% group_by(RelAbundance.y) %>% mutate(Order = seq_along(RelAbundance.y)) %>% spread(key = Order, value = NewName.y))
+### Species labels with Italicized specise name ###
+species_labels <- c(expression(paste(italic("Desmoscolex "),"sp1")),
+                    expression(paste(italic("Desmoscolex "),"sp10")),
+                    expression(paste(italic("Desmoscolex "),"sp11")),
+                    expression(paste(italic("Desmoscolex "),"sp12")),
+                    expression(paste(italic("Desmoscolex "),"sp2")),
+                    expression(paste(italic("Desmoscolex "),"sp3")),
+                    expression(paste(italic("Desmoscolex "),"sp4")),
+                    expression(paste(italic("Desmoscolex "),"sp5")),
+                    expression(paste(italic("Desmoscolex "),"sp6")),
+                    expression(paste(italic("Desmoscolex "),"sp7")),
+                    expression(paste(italic("Desmoscolex "),"sp8")),
+                    expression(paste(italic("Desmoscolex "),"sp9")))
 
-Label_DF_left_Format$FinalLabel <- paste(Label_DF_left_Format$`1`, Label_DF_left_Format$`2`, Label_DF_left_Format$`3`, Label_DF_left_Format$`4`, Label_DF_left_Format$`5`, Label_DF_left_Format$`6`, sep = ", ")
-Label_DF_left_Format$FinalLabel <- gsub("NA, ","",Label_DF_left_Format$FinalLabel) 
-Label_DF_left_Format$FinalLabel <- gsub(", NA","",Label_DF_left_Format$FinalLabel)
+### Plot using GGPLOT ### 
+PLOT_Desmoscolecidae <- Final_DF %>% ggplot() +
+  geom_segment(aes(x= 1, xend= 2, y=VSearch_Abundance, yend=DADA2_Abundance, col=factor(Species.x)), size=0.75, show.legend = T) +
+  geom_point(aes(x = 1, y = VSearch_Abundance), size = 2, col = "grey30") +
+  geom_point(aes(x = 2, y = DADA2_Abundance), size = 2, col = "grey30") +
+  geom_text(aes(label = paste0(VSearch_Abundance, "%"), y = VSearch_Abundance, x = 1 - 0.1), check_overlap = T, size = 3) +
+  geom_text(aes(label = paste0(DADA2_Abundance, "%"), y = DADA2_Abundance, x = 2 + 0.1), check_overlap = T, size = 3) +
+  geom_vline(xintercept=1, linetype="twodash", size=.5) + 
+  scale_color_manual(labels = species_labels,
+                     values=c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000')) +
+  geom_vline(xintercept=2, linetype="twodash", size=.5) +
+  geom_text(label="VSearch", x=1, y=-2, hjust=-0.2, size=4, check_overlap = T) +
+  geom_text(label="DADA2", x=2, y=-2, hjust=1.2, size=4, check_overlap = T) +
+  ylim(1,100) + labs(color='Species')
 
-Label_DF_right_Format$FinalLabel <- paste(Label_DF_right_Format$`1`, Label_DF_right_Format$`2`, Label_DF_right_Format$`3`, Label_DF_right_Format$`4`, Label_DF_right_Format$`5`, sep = ", ")
-Label_DF_right_Format$FinalLabel <- gsub("NA, ","",Label_DF_right_Format$FinalLabel) 
-Label_DF_right_Format$FinalLabel <- gsub(", NA","",Label_DF_right_Format$FinalLabel)
-
-ab_left_label <- paste(Label_DF$RelAbundance.x, "%")
-ab_right_label <- paste(Label_DF$RelAbundance.y, "%")
-
-Label_DF_left <- merge(Label_DF, Label_DF_left_Format, by = "RelAbundance.x", all = F)
-Label_DF_left_right <- merge(Label_DF_left, Label_DF_right_Format, by = "RelAbundance.y", all = F)
-
-
-PLOT_Desmoscolecidae <- ggplot(Label_DF) + geom_segment(aes(x="1", xend="2", y=RelAbundance.x, yend=RelAbundance.y, col=factor(Species.x)), size=0.75, show.legend = T) +
-  theme(panel.background = element_blank(),  panel.grid = element_blank(), axis.ticks = element_blank(),
-        axis.text = element_blank(), axis.title = element_blank(), panel.border = element_blank(), plot.margin = unit(c(1,0,1,-1), "cm"),
-        plot.title = element_text(hjust = 0.5), legend.key = element_rect(fill = "transparent", colour = "transparent"), legend.title.align = 0.5) +
-  labs(color='Species') +
-  geom_label(label=ab_left_label, y=Label_DF$RelAbundance.x, x=rep(1,NROW(Label_DF$RelAbundance.x)), size=3, hjust = 0, fontface="bold", vjust = 0, alpha=0.35, label.size = NA, label.padding = unit(0.05, "lines")) +
-  geom_label(label=ab_right_label, y=Label_DF$RelAbundance.y, x=rep(2,NROW(Label_DF$RelAbundance.y)), size=3, hjust = 1, fontface="bold", vjust = 0, alpha=0.35, label.size = NA, label.padding = unit(0.05, "lines")) +
-  geom_text(label=Label_DF_left_right$FinalLabel.x, y=Label_DF_left_right$RelAbundance.x, x=rep(1, NROW(Label_DF$RelAbundance.x)), hjust = 1, size=2.5, vjust=0, check_overlap = T) +
-  geom_text(label=Label_DF_left_right$FinalLabel.y, y=Label_DF_left_right$RelAbundance.y, x=rep(2, NROW(Label_DF$RelAbundance.y)), hjust = 0, size=2.5, vjust=0, check_overlap = T) +
-   geom_vline(xintercept=1, linetype="twodash", size=.1) + 
-  scale_color_manual(values=c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000')) +
-  geom_vline(xintercept=2, linetype="twodash", size=.1) +
-  geom_text(label="VSearch", x=1, y=102, hjust=-0.2, size=5, check_overlap = T) +
-  geom_text(label="DADA2", x=2, y=102, hjust=1.2, size=5, check_overlap = T) +
-  ylim(1,100) + ggtitle("Desmoscolecidae ASVs/OTUs")
+PLOT_Desmoscolecidae <- PLOT_Desmoscolecidae + theme(panel.background = element_blank(),  panel.grid = element_blank(), axis.ticks = element_blank(), legend.text.align = 0,
+                                                     axis.text = element_blank(), axis.title = element_blank(), panel.border = element_blank(), plot.margin = unit(c(1,0.5,1,1), "cm"),
+                                                     plot.title = element_text(hjust = 0.5), legend.key = element_rect(fill = "transparent", colour = "transparent"), legend.title.align = 0.5)
 PLOT_Desmoscolecidae
 
 ggsave("../Comparing_OTUs_ASVs/Desmoscolecidae_OTU_Corresponding_ASV.tiff", width = 16, height = 10, dpi = 300)
